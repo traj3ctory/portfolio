da5e5d47a9db1b8b41132145ffd69b559a00b53e
traj3ctory <davidbenard.bd@gmail.com>
2021-12-30 12:17:35 +0100

    feat: add filter div feature
    it isnt working yet

--- src/view/Portfolio.tsx
+++ src/view/Portfolio.tsx
@@ -8,51 +8,33 @@ import johanan from "../asset/img/jwlblog.png";
 import jwl from "../asset/img//jwl.png";
 import ebs from "../asset/img/ebs.png";
 
-// filterSelection("all");
-function filterSelection(item?: any) {
-  let c = item !== "" ? item.target.outerText : "all";
-  let i;
-  let x = document.getElementsByClassName("p-col-12");
-  // if (c === "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    if (x[i].className.includes(c)) {
-      w3AddClass(x[i], "d-block");
-      w3RemoveClass(x[i], "d-none");
-    } else {
-      w3AddClass(x[i], "d-none");
-      w3RemoveClass(x[i], "d-block");
-    }
-  }
-}
+const filters = document.querySelectorAll('.filter');
 
-// Show filtered elements
-function w3AddClass(element: Element, name: string) {
-  let i;
-  let arr1;
-  let arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) === -1) {
-      element.className += ` ${arr2[i]}`;
-    }
-  }
-}
+filters.forEach(filter => { 
 
-// // Hide elements that are not selected
-function w3RemoveClass(element: Element, name: string) {
-  let i;
-  let arr1;
-  let arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.includes(arr2[i])) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
+  filter.addEventListener('click', () => {
+
+    const selectedFilter = filter.getAttribute('data-filter');
+    let itemsToHide = document.querySelectorAll(`.projects .project:not([data-filter='${selectedFilter}'])`);
+    let itemsToShow = document.querySelectorAll(`.projects [data-filter='${selectedFilter}']`);
+
+    if (selectedFilter === 'all') {
+      itemsToHide = [];
+      itemsToShow = document.querySelectorAll('.projects [data-filter]');
     }
-  }
-  element.className = arr1.join(" ");
-}
+
+    itemsToHide.forEach(el => {
+      el.classList.add('hide');
+      el.classList.remove('show');
+    });
+
+    itemsToShow.forEach(el => {
+      el.classList.remove('hide');
+      el.classList.add('show'); 
+    });
+
+  });
+});
 
 function Portfolio() {
   // useEffect(() => {
@@ -63,26 +45,26 @@ function Portfolio() {
     <section id="portfolio">
       <Card title="Portfolio">
         <section className="portfolio section" id="portfolio">
-          <div id="myBtnContainer">
-            <button className="p-button mx-1" onClick={filterSelection}>
+          <div id="filter">
+            <button className="p-button mx-1 filter" data-filter="all">
               all
             </button>
-            <button className="p-button mx-1" onClick={filterSelection}>
+            <button className="p-button mx-1 filter" data-filter="react">
               react
             </button>
-            <button className="p-button mx-1" onClick={filterSelection}>
+            <button className="p-button mx-1 filter" data-filter="web">
               web
             </button>
-            <button className="p-button mx-1" onClick={filterSelection}>
+            <button className="p-button mx-1 filter" data-filter="wordpress">
               wordpress
             </button>
-            <button className="p-button mx-1" onClick={filterSelection}>
+            <button className="p-button mx-1 filter" data-filter="opencart">
               opencart
             </button>
           </div>
 
           <div className="p-grid" id="portfolioItem">
-            <div className="p-col-12 p-md-4 react" data-category="react">
+            <div className="p-col-12 p-md-4 react" data-filter="react">
               <img src={leerix} alt="leerix application" />
               <div className="portfolio-info">
                 <h4>Lyrics Finder</h4>
@@ -91,7 +73,7 @@ function Portfolio() {
                 </div>
               </div>
             </div>
-            <div className="p-col-12 p-md-4 web" data-category="web">
+            <div className="p-col-12 p-md-4 web" data-filter="web">
               <img src={portfolio} alt="portfolio site" />
               <div className="portfolio-info">
                 <h4>Portfolio</h4>
@@ -100,7 +82,7 @@ function Portfolio() {
                 </div>
               </div>
             </div>
-            <div className="p-col-12 p-md-4 web" data-category="web">
+            <div className="p-col-12 p-md-4 web" data-filter="web">
               <img src={sys} alt="Sysbanker EE" />
               <div className="portfolio-info">
                 <h4>Sysanker EE</h4>
@@ -111,7 +93,7 @@ function Portfolio() {
             </div>
             <div
               className="p-col-12 p-md-4 wordpress"
-              data-category="wordpress"
+              data-filter="wordpress"
             >
               <img src={uifww} alt="Utali Igbo" />
               <div className="portfolio-info">
@@ -123,7 +105,7 @@ function Portfolio() {
             </div>
             <div
               className="p-col-12 p-md-4 wordpress"
-              data-category="wordpress"
+              data-filter="wordpress"
             >
               <img src={johanan} alt="Johanan World Blog" />
               <div className="portfolio-info">
@@ -133,7 +115,7 @@ function Portfolio() {
                 </div>
               </div>
             </div>
-            <div className="p-col-12 p-md-4 opencart" data-category="opencart">
+            <div className="p-col-12 p-md-4 opencart" data-filter="opencart">
               <img src={jwl} alt="Johanan World" />
               <div className="portfolio-info">
                 <h4>Johanan World</h4>
@@ -142,7 +124,7 @@ function Portfolio() {
                 </div>
               </div>
             </div>
-            <div className="p-col-12 p-md-4 web" data-category="web">
+            <div className="p-col-12 p-md-4 web" data-filter="web">
               <img src={ebs} alt="Accsiss eBs" />
               <div className="portfolio-info">
                 <h4>Accsiss eBs</h4>
