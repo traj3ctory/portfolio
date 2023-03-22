e2a50a26f689c2a113a164382c9316083e747fd7
traj3ctory <davidbenard.bd@gmail.com>
2023-03-22 07:52:09 +0100

    feat: update

--- src/view/Portfolio.tsx
+++ src/view/Portfolio.tsx
@@ -57,8 +57,8 @@ const Portfolio: React.FC<PortfolioProps> = ({ portfolio }) => {
               </div>
               <div className="col-md-6 mb-3">
                 <h4 className="mb-3">{visible.name}</h4>
-                <small className="mb-2">...{visible.description}</small>
-                <small className="mb-2"><strong>{visible.role}</strong></small>
+                <small className="mb-2">...{visible.description}</small><hr />
+                <small className="mb-2"><strong>{visible.role}</strong></small><br />
                 <small className="mb-2">{visible.tech}</small>
                 <hr />
                 <Button
