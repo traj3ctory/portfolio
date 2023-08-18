50b6461b0fc1538fae2843577ae8a9562e840654
traj3ctory <davidbenard.bd@gmail.com>
2023-08-18 05:29:48 +0100

    SEO optimizations

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -3,15 +3,15 @@ const SocialMedia = () => (
 		<span className="find_me">Find Me on</span>
 		<ul>
 			<li>
-				<a href="https://twitter.com/dav3ly" rel="noreferrer" target="_blank">
-					{/* <img src={twitter} alt="Twitter" /> */}
-					<span className="pi pi-twitter" />
-				</a>
-			</li>
-			<li>
 				<a href="https://www.linkedin.com/in/david-benard-196961121/" rel="noreferrer" target="_blank">
 					{/* <img src={facebook} alt="Facebook" /> */}
 					<span className="pi pi-linkedin" />
+				</a>
+			</li>
+			<li>
+				<a href="https://github.com/traj3ctory" rel="noreferrer" target="_blank">
+					{/* <img src={github} alt="Github" /> */}
+					<span className="pi pi-github" />
 				</a>
 			</li>
 			<li>
@@ -21,9 +21,9 @@ const SocialMedia = () => (
 				</a>
 			</li>
 			<li>
-				<a href="https://github.com/traj3ctory" rel="noreferrer" target="_blank">
-					{/* <img src={github} alt="Github" /> */}
-					<span className="pi pi-github" />
+				<a href="https://twitter.com/dav3ly" rel="noreferrer" target="_blank">
+					{/* <img src={twitter} alt="Twitter" /> */}
+					<span className="pi pi-twitter" />
 				</a>
 			</li>
 		</ul>
