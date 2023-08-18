6a9a28e00b27f808b6f4819c50e34eb69453f5aa
traj3ctory <davidbenard.bd@gmail.com>
2023-08-18 05:31:04 +0100

    SEO optimizations

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -4,25 +4,21 @@ const SocialMedia = () => (
 		<ul>
 			<li>
 				<a href="https://www.linkedin.com/in/david-benard-196961121/" rel="noreferrer" target="_blank">
-					{/* <img src={facebook} alt="Facebook" /> */}
 					<span className="pi pi-linkedin" />
 				</a>
 			</li>
 			<li>
 				<a href="https://github.com/traj3ctory" rel="noreferrer" target="_blank">
-					{/* <img src={github} alt="Github" /> */}
 					<span className="pi pi-github" />
 				</a>
 			</li>
 			<li>
 				<a href="https://wa.link/y6l5ch" rel="noreferrer" target="_blank">
-					{/* <img src={facebook} alt="Facebook" /> */}
 					<span className="pi pi-whatsapp" />
 				</a>
 			</li>
 			<li>
 				<a href="https://twitter.com/dav3ly" rel="noreferrer" target="_blank">
-					{/* <img src={twitter} alt="Twitter" /> */}
 					<span className="pi pi-twitter" />
 				</a>
 			</li>
